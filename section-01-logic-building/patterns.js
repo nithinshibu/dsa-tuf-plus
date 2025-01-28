56d70a02529adeeeb7257ca3f83d16bbc9e93c0e
Nithin Mathew <nithinshibumathew@gmail.com>
2025-01-28 10:36:51 +0530

    logic building - patterns

---
+++ section-01-logic-building/patterns.js
@@ -0,0 +1,21 @@
+/* 
+
+n=4
+
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+
+*/
+
+function printPattern(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      process.stdout.write("*");
+    }
+    console.log();
+  }
+}
+
+printPattern(4);
