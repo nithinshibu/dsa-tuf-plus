115d24ecc27d43a0a2e44ecdf61ad1e4ad8dadf9
Nithin Mathew <nithinshibumathew@gmail.com>
2025-02-05 12:52:07 +0530

    pattern 11 - tuf approach added

--- section-01-logic-building/pattern11.js
+++ section-01-logic-building/pattern11.js
@@ -40,3 +40,47 @@ function printPattern(n) {
 }
 
 printPattern(5);
+
+
+/* 
+
+Tuf solution:-
+
+class Solution {
+    // Function to print pattern11
+    pattern11(n) {
+        // First row starts by printing a single 1.
+        let start = 1;
+
+        // Outer loop for the number of rows
+        for (let i = 0; i < n; i++) {
+            /* If the row index is even, start 
+            with 1; if odd, start with 0 
+            if (i % 2 === 0) start = 1;
+             
+            else start = 0;
+        
+
+            /* Alternatively print 1's and 0's 
+            in each row by using inner for loop
+            let row = "";
+            for (let j = 0; j <= i; j++) {
+                row += start+" ";
+                start = 1 - start;
+            }
+
+            // Output the row
+            console.log(row);
+        }
+    }
+}
+
+let N = 5;
+
+// Create an instance of Solution class
+let sol = new Solution();
+
+sol.pattern11(N);
+
+
+*/
