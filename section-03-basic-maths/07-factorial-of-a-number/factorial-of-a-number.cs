4b2497441468fc90245ce0bb7bd03f14e48ed218
Nithin Shibu Mathew <nithinshibumathew@gmail.com>
2026-03-17 15:27:44 +0530

    Factorial of a number

---
+++ section-03-basic-maths/07-factorial-of-a-number/factorial-of-a-number.cs
@@ -0,0 +1,42 @@
+using System;
+
+public class Solution
+{
+    /* Function to find the
+    factorial of a number */
+    public int Factorial(int n)
+    {
+        // Edge case
+        if(n == 0) return 1;
+        
+        // Variable to store the factorial
+        int fact = 1;
+
+        // Iterate from 1 to n
+        for (int i = 1; i <= n; i++)
+        {
+            // Multiply fact with current number
+            fact = fact * i;
+        }
+        
+        // Return the factorial stored
+        return fact;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Input number
+        int n = 4;
+
+        // Creating an instance of Solution class
+        Solution sol = new Solution();
+
+        // Function call to find the factorial of n
+        int ans = sol.Factorial(n);
+
+        Console.WriteLine("The factorial of given number is: " + ans);
+    }
+}
